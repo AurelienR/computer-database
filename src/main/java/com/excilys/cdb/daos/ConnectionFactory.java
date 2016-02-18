3dbd1050f7b49bb1f53fa85307cdacd3b563c5bb
AurelienR <aurelien.richeton@gmail.com>
2016-02-18 17:01:19 +0100

    Add poolConnection

--- src/main/java/com/excilys/cdb/daos/ConnectionFactory.java
+++ src/main/java/com/excilys/cdb/daos/ConnectionFactory.java
@@ -2,10 +2,12 @@ package com.excilys.cdb.daos;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.DriverManager;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
-import java.sql.Connection;
+
+import com.jolbox.bonecp.BoneCP;
+import com.jolbox.bonecp.BoneCPConfig;
 
 /**
  * Single entry point to DAO objects Manage Connection and driver DB connection
@@ -20,19 +22,20 @@ public class ConnectionFactory {
 	private static final String DRIVER_PROPERTY = "driver";
 	private static final String USERNAME_PROPERTY = "nomutilisateur";
 	private static final String PASSWORD_PROPERTY = "motdepasse";
+	private static final String MIN_CON_PROPERTY = "MinConnectionsPerPartition";
+	private static final String MAX_CON_PROPERTY = "MaxConnectionsPerPartition";
+	private static final String PARTITION_COUNT_PROPERTY = "PartitionCount";
 
-	private String url;
-	private String username;
-	private String password;
-
+	static BoneCP connectionPool = null;
+	
 	// Singleton
 	private static ConnectionFactory instance;
 
-	private ConnectionFactory(String url, String username, String password) {
-		this.url = url;
-		this.username = username;
-		this.password = password;
-	}
+
+	// Constructor
+    ConnectionFactory( BoneCP connectionPool ) {
+        ConnectionFactory.connectionPool = connectionPool;
+    }
 
 	/**
 	 * Get instance of ConnectionFactory object correctly initialize
@@ -61,6 +64,10 @@ public class ConnectionFactory {
 		String driver = null;
 		String nomUtilisateur = null;
 		String motDePasse = null;
+		int minConnections = 0;
+		int maxConnections = 0;
+		int partitionCount = 0;
+		BoneCP connPool = null;
 
 		// Read DAO file property
 		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
@@ -78,6 +85,9 @@ public class ConnectionFactory {
 			driver = properties.getProperty(DRIVER_PROPERTY);
 			nomUtilisateur = properties.getProperty(USERNAME_PROPERTY);
 			motDePasse = properties.getProperty(PASSWORD_PROPERTY);
+			minConnections = Integer.parseInt(properties.getProperty(MIN_CON_PROPERTY));
+			maxConnections = Integer.parseInt(properties.getProperty(MAX_CON_PROPERTY));
+			partitionCount = Integer.parseInt(properties.getProperty(PARTITION_COUNT_PROPERTY));			
 
 		} catch (IOException e) {
 			throw new DAOConfigurationException("Cannot load propertied " + PROPERTY_FILE, e);
@@ -89,12 +99,31 @@ public class ConnectionFactory {
 		} catch (ClassNotFoundException e) {
 			throw new DAOConfigurationException("Driver is missing from classpath.", e);
 		}
+		
+        try {
+        	// Set up connection pool config
+            BoneCPConfig config = new BoneCPConfig();
+
+            config.setJdbcUrl( url );
+            config.setUsername( nomUtilisateur );
+            config.setPassword( motDePasse );
+
+            config.setMinConnectionsPerPartition( minConnections );
+            config.setMaxConnectionsPerPartition( maxConnections );
+            config.setPartitionCount( partitionCount );
+
+            connPool = new BoneCP( config );
+        } catch ( SQLException e ) {
+            e.printStackTrace();
+            throw new DAOConfigurationException( "Error in configuring connection pool", e );
+        }
 
 		// Instantiate and return single DAOFactory instance
-		ConnectionFactory connectionFactory = new ConnectionFactory(url, nomUtilisateur, motDePasse);
+		ConnectionFactory connectionFactory = new ConnectionFactory(connPool);
 
 		return connectionFactory;
 	}
+	
 
 	/**
 	 * Open a connection to DB based on property
@@ -103,6 +132,6 @@ public class ConnectionFactory {
 	 * @throws SQLException
 	 */
 	public Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(url, username, password);
+		return connectionPool.getConnection();
 	}
 }
