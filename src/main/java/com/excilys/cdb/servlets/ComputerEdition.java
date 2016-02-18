ad82f898a457c674dfadbab2b682db6a95c1de3e
AurelienR <aurelien.richeton@gmail.com>
2016-02-18 15:54:39 +0100

    Update pom.xml to v0.2

---
+++ src/main/java/com/excilys/cdb/servlets/ComputerEdition.java
@@ -0,0 +1,68 @@
+package com.excilys.cdb.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.excilys.cdb.dtos.CompanyDTO;
+import com.excilys.cdb.dtos.ComputerDTO;
+import com.excilys.cdb.services.ComputerDTOService;
+
+/**
+ * Servlet implementation class ComputerEdition
+ */
+@WebServlet("/editComputer") 
+public class ComputerEdition extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ComputerEdition() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Retrieve Computer information
+		int id = Integer.parseInt(request.getParameter("id"));		
+		String nameStr = request.getParameter("computerName");
+		String introducedStr = request.getParameter("introduced");
+		String discontinuedStr = request.getParameter("discontinued");
+		
+		// Retrieve related Company
+		int companyId;		
+		CompanyDTO companyDTO;
+		try{
+			companyId = Integer.parseInt(request.getParameter("companyId"));
+			companyDTO =  new CompanyDTO(companyId, null);
+		} catch(NumberFormatException e){
+			companyDTO = null;
+		}	
+		
+		// Instanciate related ComputerDTO
+		ComputerDTO computerDTO = new ComputerDTO(id, nameStr, introducedStr, discontinuedStr, companyDTO);
+		
+		// Add computer to DB
+		ComputerDTOService.getInstance().updateComputer(computerDTO);
+		
+		// Forward toDashboard
+		response.sendRedirect("./computers");
+	}
+
+}
